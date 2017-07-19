5b89848887c928d84c7da618b372b9037927b61b
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-07-19 23:41:18 +0300

    Generated source

--- jaxrs-spec-generated/src/main/java/fi/metatavu/acgbridge/server/rest/V1Api.java
+++ jaxrs-spec-generated/src/main/java/fi/metatavu/acgbridge/server/rest/V1Api.java
@@ -3,7 +3,6 @@ package fi.metatavu.acgbridge.server.rest;
 import fi.metatavu.acgbridge.server.rest.model.BadRequest;
 import fi.metatavu.acgbridge.server.rest.model.Forbidden;
 import fi.metatavu.acgbridge.server.rest.model.InternalServerError;
-import fi.metatavu.acgbridge.server.rest.model.Transaction;
 import fi.metatavu.acgbridge.server.rest.model.Pong;
 
 import javax.ws.rs.*;
@@ -20,33 +19,21 @@ import java.util.List;
 @Api(description = "the v1 API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-07-19T23:05:24.176+03:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2017-07-19T23:41:12.800+03:00")
 
 public abstract class V1Api extends AbstractApi {
 
     @DELETE
-    @Path("/transactions/{orderId}")
-    @Consumes({ "application/json;charset=utf-8" })
-    @Produces({ "application/json;charset=utf-8" })
-    @ApiOperation(value = "Cancel transaction", notes = "Cancel transaction", response = void.class, tags={ "Transactions",  })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 204, message = "The transaction was canceled successfully.", response = void.class),
-        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = void.class),
-        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = void.class),
-        @ApiResponse(code = 500, message = "Internal server error", response = void.class) })
-    public abstract Response cancelTransaction(@PathParam("orderId") String orderId,@Context Request request);
-
-    @POST
     @Path("/transactions")
     @Consumes({ "application/json;charset=utf-8" })
     @Produces({ "application/json;charset=utf-8" })
-    @ApiOperation(value = "Creates new transaction", notes = "Creates new transaction", response = Transaction.class, tags={ "Transactions",  })
+    @ApiOperation(value = "Cancel transactions", notes = "Cancel transactions", response = void.class, tags={ "Transactions",  })
     @ApiResponses(value = { 
-        @ApiResponse(code = 200, message = "Created transaction", response = Transaction.class),
-        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = Transaction.class),
-        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = Transaction.class),
-        @ApiResponse(code = 500, message = "Internal server error", response = Transaction.class) })
-    public abstract Response createTransaction(Transaction transaction,@Context Request request);
+        @ApiResponse(code = 204, message = "The transactions were canceled successfully.", response = void.class),
+        @ApiResponse(code = 400, message = "Invalid request was sent to the server", response = void.class),
+        @ApiResponse(code = 403, message = "Attempted to make a call with unauthorized client", response = void.class),
+        @ApiResponse(code = 500, message = "Internal server error", response = void.class) })
+    public abstract Response cancelTransactions(@QueryParam("paymentStrategy") String paymentStrategy,@QueryParam("orderId") String orderId,@Context Request request);
 
     @GET
     @Path("/system/ping")
